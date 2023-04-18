9644ab0dcb2c0904817daaf216f0d0453c51cf5b
Ivan Obeso <ivan.obeso1@gmail.com>
2023-04-18 13:08:05 +0100

    connected the lambda publisher with lerna

--- packages/lambda-twitter-publisher/src/lambda-publisher.ts
+++ packages/lambda-twitter-publisher/src/lambda-publisher.ts
@@ -1,8 +1,10 @@
 import { SQSEvent } from "aws-lambda";
+import { FAndG } from "@fear-greed-bot/common";
 
 export const handler_publisher = async (event: SQSEvent) => {
   const records: any[] = event.Records;
-  console.log(records)
+  const reading: FAndG = JSON.parse(records[0].body)
+  console.log(reading)
   return {
     statusCode: 200,
     body: JSON.stringify("Reading published into twitter succesfully!"),
